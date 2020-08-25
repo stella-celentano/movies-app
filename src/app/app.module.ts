3c602818d9da31e62c9818eefac891cd776c86db
stella-celentano <stellacelentano99@gmail.com>
2020-08-25 20:53:24 -0300

    Configurando ngx toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {ToastrModule} from "ngx-toastr";
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
