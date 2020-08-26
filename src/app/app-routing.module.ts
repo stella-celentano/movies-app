cf756373fc3d33776e2cae3b66eddb32c2b5af00
stella-celentano <stellacelentano99@gmail.com>
2020-08-26 20:16:47 -0300

    Criando componente movies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('./modules/welcome/welcome.module').then(module => module.WelcomeModule)
   },
   {
+    path: 'movies',
+    loadChildren: () => import('./modules/movies/movies.module').then(module => module.MoviesModule)
+  },
+  {
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
