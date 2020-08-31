943c68793e85979482ee7e736ce559e76a90aef5
stella-celentano <stellacelentano99@gmail.com>
2020-08-31 19:59:03 -0300

    Criando o componente movie-detail

--- src/app/modules/movies/movies-routing.module.ts
+++ src/app/modules/movies/movies-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { MoviesComponent } from './movies.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MoviesComponent
+  },
+  {
+    path: 'detail/:movieName',
+    component: MovieDetailComponent
   }
 ];
 
--- src/app/modules/movies/movies.module.ts
+++ src/app/modules/movies/movies.module.ts
@@ -7,10 +7,11 @@ import { FlexLayoutModule } from "@angular/flex-layout"
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MoviesComponent } from './movies.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 
 @NgModule({
-  declarations: [MoviesComponent, MovieCardComponent],
+  declarations: [MoviesComponent, MovieCardComponent, MovieDetailComponent],
   imports: [
     CommonModule,
     MoviesRoutingModule,
