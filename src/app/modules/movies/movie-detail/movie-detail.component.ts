77dce83509d3d56dbeba74179b3591c606be3a19
stella-celentano <stellacelentano99@gmail.com>
2020-08-31 20:18:12 -0300

    Implementando navegação entre movies e movie detail

--- src/app/modules/movies/movie-detail/movie-detail.component.ts
+++ src/app/modules/movies/movie-detail/movie-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-movie-detail',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const movieName = this.activatedRoute.snapshot.params['movieName']
   }
 
 }
